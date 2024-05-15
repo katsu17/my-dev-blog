c8ec672417a12d03a3904a7ae1795046e63ff9ed
tandai katsuaki <>
2024-05-15 09:06:24 +0900

    added google ad meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css"
 import { GoogleTagManager } from "@next/third-parties/google"
 import Header from "./components/Header"
 import ArticleList from "./components/ArticleList"
+import Head from "next/head"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -13,6 +14,7 @@ export const metadata: Metadata = {
 }
 
 const GTM_TAG = process.env.GTM_TAG
+const GOOGLE_AD_PUBLISH_ID = process.env.GTM_TAG
 
 export default async function RootLayout({
   children,
@@ -22,6 +24,12 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <GoogleTagManager gtmId={`${GTM_TAG}`} />
+      <Head>
+        <meta
+          name="google-adsense-account"
+          content={`${GOOGLE_AD_PUBLISH_ID}`}
+        />
+      </Head>
       <body className={inter.className}>
         <main className="flex min-h-screen flex-col items-center justify-between p-24 text-white w-full">
           <Header />
