8222a1eaf184fe932d5e8f1865fefa91c6373ed0
tandai katsuaki <>
2024-06-13 07:44:02 +0900

    ブログ記事のデフォルト画像の設定と、microsmcからの画像読み込み設定

--- src/app/[postId]/page.tsx
+++ src/app/[postId]/page.tsx
@@ -1,6 +1,7 @@
 import { notFound } from "next/navigation"
 import parse from "html-react-parser"
 import { getDetail, getList } from "../libs/microcms"
+import Image from "next/image"
 
 export async function generateStaticParams() {
   const { contents } = await getList()
@@ -27,9 +28,21 @@ export default async function Page({
     notFound()
   }
   return (
-    <div>
-      <h1>{post.title}</h1>
-      <h2>{time}</h2>
+    <div className="max-w-[1200px]">
+      <div className="flex justify-between">
+        <h1 className="font-bold text-lg">{post.title}</h1>
+        <h2>{time}</h2>
+      </div>
+      <div className="relative ">
+        <Image
+          src={post.eyecatch ? `${post.eyecatch?.url}` : "/eyeCatchNoImage.jpg"}
+          layout="responsive"
+          //   fill={true}
+          width={600}
+          height={300}
+          alt={`${post.title}`}
+        />
+      </div>
       <div>{parse(post.content)}</div>
     </div>
   )
--- src/app/components/ArticleList/index.tsx
+++ src/app/components/ArticleList/index.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { getList } from "../../libs/microcms"
+import Image from "next/image"
 
 export default async function page() {
   const { contents } = await getList()
@@ -18,18 +19,28 @@ export default async function page() {
           return (
             <a
               href={`/${post.id}`}
-              className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+              className="group rounded-lg border border-transparent px-5 py-4"
               key={post.id}
             >
-              <h2 className="mb-3 text-2xl font-semibold">
+              <Image
+                src={
+                  post.eyecatch
+                    ? `${post.eyecatch?.url}`
+                    : "/eyeCatchNoImage.jpg"
+                }
+                width={200}
+                height={100}
+                alt={`${post.title}`}
+              />
+              <h2 className="mb-3 font-semibold">
                 {post.title}
                 <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
                   -&gt;
                 </span>
               </h2>
-              <p className="m-0 max-w-[30ch] text-sm opacity-50 truncate">
+              {/* <p className="m-0 max-w-[30ch] text-sm opacity-50 truncate">
                 {post.content.substring(21)}
-              </p>
+              </p> */}
             </a>
           )
         })}
